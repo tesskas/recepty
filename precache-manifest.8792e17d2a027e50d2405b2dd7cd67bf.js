9f077ad918de5ade8b0103500871e6111360896a
Tess <tereza.skutova@seznam.cz>
2020-09-02 09:43:03 +0200

    Updates

---
+++ precache-manifest.8792e17d2a027e50d2405b2dd7cd67bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b3c97dcfa4df159a1817a0b6c2983b",
+    "url": "/testing-react-app/index.html"
+  },
+  {
+    "revision": "a5bd836ebeb0dcc21513",
+    "url": "/testing-react-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8ba9d5165bcac7e23dad",
+    "url": "/testing-react-app/static/css/main.9d2fd7ad.chunk.css"
+  },
+  {
+    "revision": "a5bd836ebeb0dcc21513",
+    "url": "/testing-react-app/static/js/2.9457fc57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testing-react-app/static/js/2.9457fc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba9d5165bcac7e23dad",
+    "url": "/testing-react-app/static/js/main.c7dbf874.chunk.js"
+  },
+  {
+    "revision": "a60da947b27c9311de00",
+    "url": "/testing-react-app/static/js/runtime-main.73af2d04.js"
+  }
+]);
