ffa4c536eecbd0b49d1c691150f082fec56a9f08
Tess <tereza.skutova@seznam.cz>
2020-09-02 17:14:31 +0200

    Updates

---
+++ precache-manifest.a1e52933710a0fd228633e340d114fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a3c0c57e4b12eb8734efa32c09aa49",
+    "url": "/recepty/index.html"
+  },
+  {
+    "revision": "70361c719f9f94ea22d5",
+    "url": "/recepty/static/css/2.b69f272b.chunk.css"
+  },
+  {
+    "revision": "1c97f8343df0ee47b104",
+    "url": "/recepty/static/css/main.ddf89f4b.chunk.css"
+  },
+  {
+    "revision": "70361c719f9f94ea22d5",
+    "url": "/recepty/static/js/2.ee8b4bd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recepty/static/js/2.ee8b4bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c97f8343df0ee47b104",
+    "url": "/recepty/static/js/main.804dee9b.chunk.js"
+  },
+  {
+    "revision": "e71938187e14d9d78461",
+    "url": "/recepty/static/js/runtime-main.c9b7d877.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testing-react-app/precache-manifest.5ac6ec6987389cd83592a68b2eab82a6.js"
+  "/recepty/precache-manifest.a1e52933710a0fd228633e340d114fc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testing-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recepty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
