a47e44912ed7e356ddd487e3d767e3437607dc40
tesskas <36575342+tesskas@users.noreply.github.com>
2020-09-02 13:06:12 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './App.module.css';
 import Filter from "../Filter/Filter";
 import List from "../List/List";
-import {getFoods} from '../../api';
+import {callApi, parse} from '../../api';
 import Form from "../Form/Form";
 import Detail from "../Detail/Detail";
 
@@ -18,8 +18,10 @@ class App extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            foods: []
+            foods: [],
+            go: ""
         }
+        callApi("/").then(data => {this.setState({go: parse(data)})})
     }
 
     /*load =()=> {
@@ -32,6 +34,7 @@ class App extends React.Component{
     render(){
         return(
             <Router>
+                <h1>{this.state.go}</h1>
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03"
                             aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
